3dbd485eb13156dc4027aed30329a8ab834b19b1
aninovo <aninovo22@gmail.com>
2023-08-04 16:15:08 +0300

    Mock variable is moved into a separate file

--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -4,39 +4,8 @@ import {
   noteCategories,
   NoteModel,
 } from './notes.interface';
+import mock from './notes.mock-data';
 import { extractDatesFromString } from './notes.util';
-
-const mock: Array<NoteModel> = [
-  {
-    id: 0,
-    name: 'Shopping list',
-    creationDate: new Date(Date.now()),
-    category: 'Task',
-    description: 'Tomatoes, bread',
-    dates: [],
-    archived: false,
-  },
-  {
-    id: 1,
-    name: 'The theory of evolution',
-    creationDate: new Date(Date.now()),
-    category: 'Random Thought',
-    description:
-      'The theory of evolution is a biological theory that explains how living things on Earth have their origin in other preexisting types and how they change over time.',
-    dates: [],
-    archived: false,
-  },
-
-  {
-    id: 2,
-    name: 'New feature',
-    creationDate: new Date(Date.now()),
-    category: 'Idea',
-    description: 'Implement a new feature by 3/5/2023; moved from 3/4/2023',
-    dates: [new Date(2023, 5, 3), new Date(2023, 4, 3)],
-    archived: false,
-  },
-]; // Database mock
 
 @Injectable()
 export class NotesService {
